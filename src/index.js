71439421e36b508c1693fcb7d172bcd5a6d185e0
GyuriKim <artgr2062@konkuk.ac.kr>
2022-08-22 19:06:24 +0900

    #1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
+import { createStore } from "redux";
+
 const addBtn = document.querySelector("#add");
 const minusBtn = document.querySelector("#minus");
 const num = document.querySelector("span");
 
-let count = 0;
-num.innerText = count;
-const handleAdd = () => {
-  count++;
-  num.innerText = count;
+const countModifer = (count = 0) => {
+  // modify state...
+  return count;
 };
-const handleMinus = () => {
-  count--;
-  num.innerText = count;
-};
-addBtn.addEventListener("click", handleAdd);
-minusBtn.addEventListener("click", handleMinus);
+const countStore = createStore(countModifer);
